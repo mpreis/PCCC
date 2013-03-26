1a9e06cdbad5fd48c9b667ef0d3fff78a525a47b
mpreis <mario.preishuber@stud.sbg.ac.at>
2013-03-26 16:16:29 +0100

    bugfix

--- scanner.c
+++ scanner.c
@@ -12,14 +12,14 @@ struct token {
 };
 
 void initToken(struct token *t, int tokenNr, int value, char string[]) {
+	int i = 0;
 	t->id = tokenNr;
 	t->digitValue = value;
-
-	int i = 0;
 	while(i < 32 && string != NULL && string[i] != '\0') {
 		t->valueStr[i] = string[i];
 		i = i + 1;
 	}
+	t->valueStr[i] = '\0';
 }
 
 int isLetter(char c) {
@@ -28,8 +28,7 @@ int isLetter(char c) {
 }
 
 int isDigit(char c) {
-	if(c < '0' || c > '9') return 1;
-	return 0;
+	return (c >= '0' && c <= '9');
 }
 
 int getNumber(char actNumber, FILE *stream) {
@@ -45,67 +44,70 @@ int getNumber(char actNumber, FILE *stream) {
 
 struct token getToken(char c, FILE *stream) {
 	struct token t;
-	initToken(&t, -1, -1, NULL);
-	if(c == '[') initToken(&t, 1, -1, NULL);
-	if(c == ']') initToken(&t, 2, -1, NULL);
-	if(c == '(') initToken(&t, 3, -1, NULL);
-	if(c == ')') initToken(&t, 4, -1, NULL);
-	if(c == '{') initToken(&t, 5, -1, NULL);
-	if(c == '}') initToken(&t, 6, -1, NULL);
-	if(c == ';') initToken(&t, 7, -1, NULL);
-	if(c == ',') initToken(&t, 8, -1, NULL);
-	if(c == '\"')initToken(&t, 9, -1, NULL);
-	if(c == '\'')initToken(&t, 10, -1, NULL);
-	if(c == '=') initToken(&t, 11, -1, NULL);
-	if(c == '+') initToken(&t, 12, -1, NULL);
-	if(c == '-') {					/* differ: numbers < 0, arithmetrical operation and -> */
+		 if(c == '[') initToken(&t, 1, -1, NULL);
+	else if(c == ']') initToken(&t, 2, -1, NULL);
+	else if(c == '(') initToken(&t, 3, -1, NULL);
+	else if(c == ')') initToken(&t, 4, -1, NULL);
+	else if(c == '{') initToken(&t, 5, -1, NULL);
+	else if(c == '}') initToken(&t, 6, -1, NULL);
+	else if(c == ';') initToken(&t, 7, -1, NULL);
+	else if(c == ',') initToken(&t, 8, -1, NULL);
+	else if(c == '\"')initToken(&t, 9, -1, NULL);
+	else if(c == '\'')initToken(&t, 10, -1, NULL);
+	else if(c == '=') initToken(&t, 11, -1, NULL);
+	else if(c == '+') initToken(&t, 12, -1, NULL);
+	else if(c == '-') {					/* differ: numbers < 0, arithmetrical operation and -> */
 		int number;
 		char nc = fgetc(stream);
 		if(nc == ' ') initToken(&t, 13, -1, NULL);
-		if(nc == '>') initToken(&t, 37, -1, NULL);
-		if(isDigit(nc)) {
+		else if(nc == '>') initToken(&t, 37, -1, NULL);
+		else if(isDigit(nc)) {
 			int number = getNumber(nc, stream);
 			number = number * -1;
 			initToken(&t, 30, number, NULL);
 		}
 	}	
-	if(c == '*') {					/* differ: pointer, arithmetrical operation */
+	else if(c == '*') {					/* differ: pointer, arithmetrical operation */
 		char nc = fgetc(stream);
 		if(nc == ' ') initToken(&t, 14, -1, NULL);
 		if(isLetter(nc)) 
 			initToken(&t, 38, -1, NULL);
 	}
-	if(c == '/') {					/* differ: comment, division */
+	else if(c == '/') {					/* differ: comment, division */
 		char nc = fgetc(stream);
 		if(nc == ' ') initToken(&t, 15, -1, NULL);
-		if(nc == '*') {
+		else if(nc == '*') {
 			nc = fgetc(stream);
-			while(nc != '*' && (nc = fgetc(stream)) != '/' && feof(stream) == 0) {}
+			char nnc = fgetc(stream);
+			while(nc != '*' && nnc != '/' && feof(stream) == 0) {
+				nc = nnc;
+				nnc = fgetc(stream);
+			}
 			initToken(&t, 35, -1, NULL);
 		}
 	}
-	if(c == '<') initToken(&t, 16, -1, NULL);	/* also used for include, handled seperatly */
-	if(c == '>') initToken(&t, 17, -1, NULL);	/* also used for include, handled seperatly */
-	if(c == '\n')initToken(&t, 42, -1, NULL);
-	if(c == '&') { 					/* do and method*/
+	else if(c == '<') initToken(&t, 16, -1, NULL);	/* also used for include, handled seperatly */
+	else if(c == '>') initToken(&t, 17, -1, NULL);	/* also used for include, handled seperatly */
+	else if(c == '\n')initToken(&t, 42, -1, NULL);
+	else if(c == '&') { 					/* do and method*/
 		char nc = fgetc(stream);
 		if(nc == '&') initToken(&t, 22, -1, NULL);
-		if(isLetter(c)) 
+		else if(isLetter(c)) 
 			initToken(&t, 36, -1, NULL);
 	}
-	if(c == '#') {	 				/* do include method, 40 <...> und 41 "..." */
+	else if(c == '#') {	 				/* do include method, 40 <...> und 41 "..." */
 		
 	}
-	if(isDigit(c)) { 				/* do number method*/
+	else if(isDigit(c)) { 				/* do number method*/
 		int number = getNumber(c, stream);
 		initToken(&t, 30, number, NULL);
 	}
-	if(isLetter(c)) {
+	else if(isLetter(c)) {
 		
 	}
-	
+	else
+		initToken(&t, -1, -1, NULL);
 	return t;
-
 }
 
 
@@ -130,12 +132,13 @@ int main(int argc, char *argv[]){
 	}
 
 	while( feof(stream) == 0 ) {
+		printf("\n*** eof:%i", feof(stream));
 		/* read character */
 		char c = fgetc(stream);
-
+		printf("\nc:%c(%i)", c,((int)c));		
 		/* check input-character */
 		t = getToken(c, stream);
-		printf("%i-\"%s\"-\'%i\'\n", t->id, t->valueStr, t->digitValue);
+		printf("\n%i   %s   %i \n", t.id, t.valueStr, t.digitValue);
 	}
 	if(fclose(stream) != 0)
 		printf("\nWARNING: can not close file!\n");	
