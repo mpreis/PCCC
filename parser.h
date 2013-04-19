0491ccc277200b5751236ab0bad168e7478e0ee1
thuetter <thomas.huetter@stud.sbg.ac.at>
2013-04-19 15:37:56 +0200

    parser.h

---
+++ parser.h
@@ -0,0 +1,11 @@
+/*
+ * PSEUDOCODE - parser.h
+ * authors: thomas huetter 1120239, mario preishuber 1120643
+ *
+ */
+void startParse();
+int identifier();
+int command();
+int expression();
+int ifCmd();
+int procCall();
